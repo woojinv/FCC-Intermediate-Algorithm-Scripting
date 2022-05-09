d4aafb98f0f195396ceb1752b717d7f2ec62fee5
woojinv <oh.woojin95@gmail.com>
2022-05-08 23:06:35 -0500

    formatting

--- 1-sum-all-numbers-in-a-range.js
+++ 1-sum-all-numbers-in-a-range.js
@@ -1,19 +1,19 @@
 // link: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/sum-all-numbers-in-a-range
 
 function sumAll(arr) {
-    let sum = 0;
-    const sortedArr = arr.sort((a, b) => a - b);
-    for (let i = sortedArr[0]; i <= sortedArr[1]; i++) {
-      sum += i;
-    }
-  
-    return sum;
+  let sum = 0;
+  const sortedArr = arr.sort((a, b) => a - b);
+  for (let i = sortedArr[0]; i <= sortedArr[1]; i++) {
+    sum += i;
   }
-  
-  sumAll([1, 4]);
-  
-  // I'm given an array of two numbers in randomg order. Return the sum of all the numbers in the range of the two given numbers, inclusive. 
-  
-  // 1. sort the given array ascending.
-  // 2. use a for loop. 
-  // url: 
+
+  return sum;
+}
+
+sumAll([1, 4]);
+
+// I'm given an array of two numbers in randomg order. Return the sum of all the numbers in the range of the two given numbers, inclusive.
+
+// 1. sort the given array ascending.
+// 2. use a for loop.
+// url:
--- 3-seek-and-destroy.js
+++ 3-seek-and-destroy.js
@@ -1,15 +1,15 @@
 // link: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/seek-and-destroy
 
 function destroyer(arr, ...args) {
-    args.forEach(arg => arr = arr.filter(item => item !== arg));
-    return arr;
-  }
-  
-  destroyer([1, 2, 3, 1, 2, 3], 2, 3);
-  
-  // I'm given an array and a variable number of arguments. I have to remove the items from the array that match the value of the arguments I'm given.
-  
-  /*
+  args.forEach((arg) => (arr = arr.filter((item) => item !== arg)));
+  return arr;
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+// I'm given an array and a variable number of arguments. I have to remove the items from the array that match the value of the arguments I'm given.
+
+/*
   1. Use the spread operator to create an array of the arguments i'm given.
   2. loop through the args array
   3. filter out the values in the given array that match the values in the arguments array. 
